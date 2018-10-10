ac6ed0cfffb6487df903f206d7946c3b8779f31c
YUZE1995\yuze1 <zxc24630918@gmail.com>
2018-10-10 22:41:23 +0800

    add counter.vue

---
+++ src/views/Counter.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h3>It's a Counter.</h3>
+    <p>
+      Count: {{ $store.state.count  }}
+    </p>
+    <button @click="addCount"> + </button>
+    <button @click="minusCount"> - </button>
+    <button @click="showCount"> Show Count </button>
+    <p>
+      {{ $store.state.errorMsg }}</p>
+  </div>
+</template>
+
+<script>
+import { mapActions } from 'vuex'
+
+export default {
+  data () {
+    return {
+      count: 0,
+      errorMsg: ''
+    }
+  },
+  methods: {
+    ...mapActions([
+      'addCount', 'minusCount'
+    ]),
+    showCount: function () {
+      alert(this.$store.state.count)
+    }
+  }
+}
+</script>
+
+<style>
+</style>
